bb3e048b10e66e2699cf8e4f7cbe1ec6b9a63383
manmeetdevgun <manmeet.devgun@nagarro.com>
2021-02-18 23:25:47 +0530

    update gitignore

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO PRICING (`service_id`, `price`, `discount`,`on_inspection`) VALUES (1, 1000.0, 0, 0);
+INSERT INTO PRICING (`service_id`, `price`, `discount`,`on_inspection`) VALUES (2, 1400.0, 10, 0);
+INSERT INTO PRICING (`service_id`, `price`, `discount`,`on_inspection`) VALUES (3, 1500.0, 20, 0);
+INSERT INTO PRICING (`service_id`, `price`, `discount`,`on_inspection`) VALUES (4, 1600.0, 30, 0);
+INSERT INTO PRICING (`service_id`, `price`, `discount`,`on_inspection`) VALUES (5, 1700.0, 25, 0);
