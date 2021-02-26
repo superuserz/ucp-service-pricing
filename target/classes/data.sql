d9e095da2d7962b58f6a8d3c2d5fa62af7233d67
superuserz <manmeet.devgun@yahoo.in>
2021-02-26 14:10:15 +0530

    updated

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO PRICING (`service_id`, `price`, `discount`,`on_inspection`) VALUES (1, 1000.0, 0, 0);
-INSERT INTO PRICING (`service_id`, `price`, `discount`,`on_inspection`) VALUES (2, 1400.0, 10, 0);
-INSERT INTO PRICING (`service_id`, `price`, `discount`,`on_inspection`) VALUES (3, 1500.0, 20, 0);
-INSERT INTO PRICING (`service_id`, `price`, `discount`,`on_inspection`) VALUES (4, 1600.0, 30, 0);
